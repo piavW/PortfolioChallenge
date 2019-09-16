f609ca91450d2018789e32a99e3ac2226488ec79
piavw <piavonwachenfelt@hotmail.com>
2019-09-16 15:33:07 +0200

    Creates folders about and project, links them to the index.js. Installs and adds BrowserRouter.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import Hello from './Hello'
 import Header from './Header'
 import Footer from './Footer'
+import { BrowserRouter } from 'react-router-dom'
+import Projects from './Projects'
+import About from './About'
 
 const App = () => {
     return (
