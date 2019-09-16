dd23d955352b33645e409d43612ceaf69e7714a7
piavw <piavonwachenfelt@hotmail.com>
2019-09-16 15:06:08 +0200

    Changes name of index.js to App.js, creates Footer and Header, links Footer and Header to App.js.

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>Made with React by Pia von Wachenfelt</h1>
+        </footer>
+    )
+}
+
+export default Footer
