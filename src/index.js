3792695b5eaf41c0314261bb4c888a30c9101f50
piavw <piavonwachenfelt@hotmail.com>
2019-09-16 15:51:04 +0200

    Adds BrowserRouters Switch and Route components. Adds header navlink and link to About and Portfolio.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,24 @@ import Footer from './Footer'
 import { BrowserRouter } from 'react-router-dom'
 import Projects from './Projects'
 import About from './About'
+import { Switch, Route } from 'react-router-dom'
 
 const App = () => {
     return (
-        <div>
+        <>
             <Header />
-            <Hello />
+            <Switch>
+                <Route exact path='/' component={Hello}></Route>
+                <Route exact path='/about' component={About}></Route>
+                <Route exact path='/projects' component={Projects}></Route>
+            </Switch>
             <Footer />
-        </div>
+        </>
     )
 };
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(( 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('app'));
