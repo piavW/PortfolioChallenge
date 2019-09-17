6a2eeb08a74b436b9242706bf8198d794eeb68b2
piavw <piavonwachenfelt@hotmail.com>
2019-09-17 16:08:38 +0200

    Adds undraw illustrations.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import axios from "axios"
 import ProjectCard from "./ProjectCard"
+import { UndrawDesignerLife } from "react-undraw-illustrations";
 
 class Projects extends Component {
      constructor() {
@@ -31,10 +32,18 @@ class Projects extends Component {
         }
         return (
             <div className="ui main container">
-                <h1 className="ui header">My Projects</h1>
-                <div className="ui stackable four column grid">
-                    {projectsList}
-                </div>
+                <div className="ui stackable two column grid">
+                    <div className="column">
+                    <UndrawDesignerLife />
+                    </div>
+                    <div className="column">
+                        <h1 className="ui header">My Projects</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia quod ab doloremque eaque. Consequatur temporibus, quos enim, eaque nemo ad iusto sequi modi totam qui veniam?</p>
+                        </div>
+                    </div>
+                    <div className="ui stackable four column grid">
+                        {projectsList}
+                    </div>
             </div>
         )
     }
