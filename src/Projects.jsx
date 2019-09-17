37b994a13fe437db821cde7849b80bf39894a372
piavw <piavonwachenfelt@hotmail.com>
2019-09-17 13:51:47 +0200

    Adds functionality to getting projects info from axios. ProjectsList shows in server.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,44 +1,42 @@
 import React, { Component } from "react"
-import axios from 'axios'
+import axios from "axios"
 
  class Projects extends Component {
      constructor() {
          super();
          this.state = {
-             projects: [
-                 {
-                     "id": 1,
-                     "name": "My First Website"
-                 }, 
-                 {
-                    "id": 2,
-                    "name": "FizzBuzz" 
-                 }
-             ]
+             projects:[]
          };
      }
-     render() {
+     componentDidMount() {
+        axios.get('./src/data/projects.json')
+        .then(response => {
+            this.setState({
+                projects: response.data
+            })
+        })
+    }
+    render() {
          const projects = this.state.projects
          let projectsList
          if (projects.length > 0) {
              projectsList = projects.map(project => {
                 return (
-                    <div key={projects.id}>
+                    <div key={project.id}>
                         <h3 className="ui header">
-                        {projects.name}
+                        {project.name}
                         </h3>
                     </div>
                 )
-             })
-         }
-         return (
+            })
+        }
+        return (
             <div className="ui main container">
                 <h1 className="ui header">My Projects</h1>
-                <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
                 {projectsList}
             </div>
-         )
-     }
+        )
+    }
 };
 
 export default Projects
