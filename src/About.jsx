6346ea90b9c2aae91cb100f3e50a2ecb9e472e38
piavw <piavonwachenfelt@hotmail.com>
2019-09-19 10:42:16 +0200

    Adds image to aboutpage, no need to install webpack loader.

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { UndrawFreelancer} from "react-undraw-illustrations";
-import happyskydiving from "./img/happyskydiving.jpg";
+import { UndrawFreelancer} from "react-undraw-illustrations"
 
 const About = () => {
   return (
@@ -11,10 +10,10 @@ const About = () => {
             </div>
             <div class="column">
         <h1 className="ui header" >About Me</h1>
-        <p>This year I dediced to spread my wings and take up programming. I've spent the past 9 years as a socionom/social worker and am currently becoming a junior fullstack web developer through the [Craft Academy Bootcamp](https://craftacademy.se/english/). 
-    I'm enthustiastic, positive and love a challenge, during my freetime I work on coding-projects, scuba-dive, exercise and always try to get out of my comfortzone. </p>
+        <p>This year I dediced to spread my wings and take up programming. I've spent the past 9 years as a socionom/social worker and am currently becoming a junior fullstack web developer through the Craft Academy Bootcamp. </p>
+    <p>I'm enthustiastic, positive and love a challenge, during my freetime I work on coding-projects, scuba-dive, exercise and always try to get out of my comfortzone. </p>
     <div>
-      <img src={happyskydiving.jpg} alt="skydiving-image"/>
+    <img class="ui rounded image" src={"./src/img/skydiventrance.jpg"} alt="skydiving-image"/> 
     </div>
         </div>
         </div>
