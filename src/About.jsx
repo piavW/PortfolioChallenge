f6381445e842255540b727679990f089a19243f4
piavw <piavonwachenfelt@hotmail.com>
2019-09-19 10:17:33 +0200

    Attempting to add local picture to Aboutpage, committing before trying to install webpack loader.

--- src/About.jsx
+++ src/About.jsx
@@ -1,5 +1,6 @@
-import React from "react"
-import { UndrawFreelancer} from "react-undraw-illustrations"
+import React from "react";
+import { UndrawFreelancer} from "react-undraw-illustrations";
+import happyskydiving from "./img/happyskydiving.jpg";
 
 const About = () => {
   return (
@@ -10,7 +11,11 @@ const About = () => {
             </div>
             <div class="column">
         <h1 className="ui header" >About Me</h1>
-        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+        <p>This year I dediced to spread my wings and take up programming. I've spent the past 9 years as a socionom/social worker and am currently becoming a junior fullstack web developer through the [Craft Academy Bootcamp](https://craftacademy.se/english/). 
+    I'm enthustiastic, positive and love a challenge, during my freetime I work on coding-projects, scuba-dive, exercise and always try to get out of my comfortzone. </p>
+    <div>
+      <img src={happyskydiving.jpg} alt="skydiving-image"/>
+    </div>
         </div>
         </div>
     </div>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -38,7 +38,7 @@ class Projects extends Component {
                     </div>
                     <div className="column">
                         <h1 className="ui header">My Projects</h1>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia quod ab doloremque eaque. Consequatur temporibus, quos enim, eaque nemo ad iusto sequi modi totam qui veniam?</p>
+                        <p>Here are a selection of projects I've done, click the name to get to them. There are more to come!</p>
                         </div>
                     </div>
                     <div className="ui stackable four centered column grid">
